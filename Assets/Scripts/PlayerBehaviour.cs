e8b2847e30b30d8135b9136ee2fd471d4260795c
Anderson <andersongarciavarillas@gmail.com>
2022-07-27 15:24:51 -0500

    Player movement

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,13 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();   
+    }
+
+    void Movement()
+    {
+        float h_axis = Input.GetAxis("Horizontal");
+        Vector2 vDir = new Vector2(h_axis,0f);
+        transform.Translate(vDir * speed * Time.deltaTime);
     }
 }
